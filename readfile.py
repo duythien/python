0a79f52c49a91a84e04e075d1ac80146c6da4aa6
duythien <fcduythien@gmail.com>
2013-04-18 10:02:23 -0300

    readfile.py
    rg

---
+++ readfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+from sys import argv
+script,filename = argv
+txt = open(filename)
+print 'here is your file name %r' %filename
+print txt.read()
+
+print 'type the filename again'
+file_again =raw_input("> ")
+txt_again = open(file_again)
+print txt_again.read()
