bdcdcf5c4b021b99aec92172d17631f0c569938c
Thien Tran <fcduythien@gmail.com>
2016-01-22 17:56:48 +0700

    Add sroce

--- physics/signal.py
+++ physics/signal.py
@@ -5,9 +5,17 @@ from scipy import signal
 t = np.linspace(0, 5, 100)
 x = t + np.random.normal(size = 100)
 
-#print t
+print t
 #print x
 #
-pl.plot(t,x, linewidth = 3)
-pl.plot(t, signal.detrend(x), linewidth = 3)
-pl.show()
+# pl.plot(t,x, linewidth = 3)
+# pl.plot(t, signal.detrend(x), linewidth = 3)
+# pl.show()
+# Write chunks of text data
+f = open('somefile.txt', 'wt')
+for tt in t:
+	
+		#f.write(print t)
+		#f.write(x)
+		#print(t, file =f)
+		f.write("%s     %s \n" %(tt, 2*tt))
--- physics/write-data.py
+++ physics/write-data.py
@@ -6,3 +6,7 @@ t = np.linspace(0, 5, 100)
 f = open('somefile.txt', 'wt')
 for tt in t:
 	f.write("%s %s \n" %(tt, 2*tt))
+
+# Another method to write data:
+for tt in t:
+	#print ('{0:2f} {1:2f} {2:2f}'.format(tt, tt*tt, tt*tt*tt))
